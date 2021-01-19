62b3ad6070c2cf901e5b1034265db9231baab2ea
jumpingrock <kennethyeongky@gmail.com>
2021-01-19 23:46:21 +0800

    first commit, working led project

---
+++ simpleLedProj/LED.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print("LED ON")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print("LED OFF")
+GPIO.output(18,GPIO.LOW)
